6da2896fb4d6eb5c67be68314acb74436cbea811
Aiym <i.s.s.a.i.y.m.03@gmail.com>
2023-10-06 20:53:48 +0600

    update main.py
    if else added

--- main.py
+++ main.py
@@ -10,5 +10,14 @@ while True:
     dist = ultrasonic.distance * 100
     span = pot.value * 100
     dist, span = round (dist, 3), round (span, 3)
+    if dist <= span:
+        print(f"scaled span: {span}, dist: {dist}")
+        timestamp = datetime.now().strftime('%Y/%m/%d %HH %MM %SS')
+        data = f"{timestamp} -->" \
+               f"distance : {dist} , span: {span} \n"
+        file.write(data)
 
+    else:
+        print(f"Distance > span!! scaled span : {span}, dist: {dist}")
+        sleep(1)
 file.close()
