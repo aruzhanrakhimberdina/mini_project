cb5029c4df58dea9e2b7e4d1e382ae84370f82be
akbota <akbotakajratbek@gmail.com>
2023-10-06 19:28:16 +0600

    Update main.py
    This process converts ultrasonic sensor and potentiometer measurements.

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ from datetime import datetime
 pot = MCP3008(7)
 ultrasonic = DistanceSensor (echo = 21, trigger = 20)
 file = open("D:/Users/Aruzhan/Desktop/distance_log.txt", "w")
+
+while True:
+    dist = ultrasonic.distance * 100
+    span = pot.value * 100
+    dist, span = round (dist, 3), round (span, 3)
+
+file.close()
