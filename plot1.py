adffe59769c8ef6ea20740ed94b06e14e3f56496
aruzhanrakhimberdina <99319593+aruzhanrakhimberdina@users.noreply.github.com>
2023-10-07 23:23:04 +0600

    Update plot1.py

--- plot1.py
+++ plot1.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
-import pit
+
 
 data = {
-        "x" : [i for i in range(1, 10)],
-        "y" : [i * i for i in range (1, 10)]
+        "x": [i for i in range(1, 10)],
+        "y": [i * i for i in range (1, 10)]
         }
-pit.plot(
+plt.plot(
         data["x"],data["y"],
         )
 plt. show()
